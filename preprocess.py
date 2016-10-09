ccfe85461a44ac4c375d61d61e3e03b9348c62fe
ramaprakoso <rama.wiedprakoso@gmail.com>
2016-10-09 21:13:24 +0700

    upgrade system

--- preprocess.py
+++ preprocess.py
@@ -1,38 +1,41 @@
-import nltk
-import re 
+import re,string
 from nltk.tokenize import word_tokenize
-
-class Preprocess:
-	def __init__(self): 
-		self.simpan=[]
-	def buka_file(self,filename): 
+#variabel global 
+filename='tweet1clean.csv'
+fname='stopword.txt'
+class Preproses: 
+	def __init__(self):
+		pass
+	def get_token(self,filename,fname):
+		#open file tweet 
 		with open(filename,"r") as f:
-			data=f.read().split('\n')	
-			return data
-	def hapus_url(self,fdata):
-		tampung=[]
-		for text in fdata: 
-			result = re.sub(r"http\S+", "", text)
-			return tampung.append(result)
-	def stopword(self,fname):
-		stop=[unicode(x.strip(),'utf-8') for x in open(fname,'r').read().split('\n')]
-		return stop
-	def tokenize(self,data,stoped):
-		#memilih stopwords 
-		tampung=[]
-		token=[word_tokenize(i) for i in data]
-		for a in token : 
-			for kata in a: 
-				if kata not in stoped: 
-					tampung.append(kata)
-		return tampung
+			data=f.read().split('\n')
+		#open file kbba
+	
+			#remove url 
+			tampung=[]
+			for text in data: 
+				rm=re.sub(r"http\S+", "", text)
+				result=' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)"," ",rm).split())
+				tampung.append(result)
+			#stopword removing with token  
+			stop=[unicode(x.strip(),'utf-8') for x in open(fname,'r').read().split('\n')]
+			t=[]
+			token=[word_tokenize(i) for i in tampung]
+			for a in token: 
+				for kata in a: 
+					if kata not in stop: 
+						t.append(kata)
+			#slang 
+		with open('kbba.txt','r') as k : 
+			kbba=k.read().split('\n')	
+			dic={}
+			for i in kbba: 
+				(key,val)=i.split('\t')
+				dic[str(key)]=val
+			print dic
+			
 if __name__=='__main__': 
-	b=Preprocess()
-	data=b.buka_file('tweet1clean.csv')
-	hapus=b.hapus_url(data)
-	stop=b.stopword('stopword.txt')
-	b.tokenize(hapus,stop)
-	"""data=b.buka_file()
-	stop=b.stopword('stopword.txt')
-	tokenize(data,stop)"""
+	b=Preproses()
+	print b.get_token(filename,fname)
 		
